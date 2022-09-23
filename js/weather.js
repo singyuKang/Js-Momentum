11e6d1b4ada1a614f503b783dbe79f30b4683fda
singyuKang <rkdtlscks123@naver.com>
2022-09-23 10:35:17 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -12,8 +12,8 @@ function onGeoOk(position) {
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
-      // console.log(data.weather[0].main);
-      // console.log(data.name);
+      console.log(data.weather[0].main);
+      console.log(data.name);
       weather.innerText = data.weather[0].main;
       city.innerText = data.name;
     });
